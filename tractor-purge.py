648911f1a29124b1a004b3c68e9c00902178acac
Fredrik Averpil <fredrik.averpil@gmail.com>
2017-01-19 20:12:37 +0100

    Fix various formatting

--- tractor-purge.py
+++ tractor-purge.py
@@ -11,28 +11,27 @@ import logging
 from optparse import OptionParser
 
 
-
 ####################################
 # Option parser and constants
 TRACTOR_PURGE_VERSION = 'v1.0.0'
 parser = OptionParser(version='%prog ' + TRACTOR_PURGE_VERSION)
 parser.add_option('-t', '--tq', dest='tq',
-                default='/opt/pixar/Tractor-2.2/bin/tq',
-                help='Absolute path to tq [default: %default]')      
+                  default='/opt/pixar/Tractor-2.2/bin/tq',
+                  help='Absolute path to tq [default: %default]')
 parser.add_option('-c', '--cmdlogsdir', dest='cmdlogsdir',
-                default='/var/spool/tractor/cmd-logs',
-                help='Absolute path to cmd-logs dir [default: %default]')
+                  default='/var/spool/tractor/cmd-logs',
+                  help='Absolute path to cmd-logs dir [default: %default]')
 parser.add_option('-l', '--log', dest='logfile',
-                default='/var/tmp/tractor-purge.log',
-                help='Absolute path to tractor-purge log file [default: %default]')
+                  default='/var/tmp/tractor-purge.log',
+                  help='Absolute path to tractor-purge log file [default: %default]')
 parser.add_option('-d', '--days', dest='days', default='30',
-                help='Number of days worth of jobs/logs to keep [default: %default]')
+                  help='Number of days worth of jobs/logs to keep [default: %default]')
 parser.add_option('--deletejobs', action='store_true', dest='deletejobs',
-                default=False,
-                help='Delete jobs from psql database after log deletion')
+                  default=False,
+                  help='Delete jobs from psql database after log deletion')
 parser.add_option('--dryrun', action='store_true', dest='dryrun',
-                default=False,
-                help='Do not perform actual deletion, instead just preview \
+                  default=False,
+                  help='Do not perform actual deletion, instead just preview \
                       deletions')
 (options, args) = parser.parse_args()
 
@@ -49,10 +48,10 @@ DRY_RUN = options.dryrun
 
 # Logging
 logger = logging.getLogger('Tractor 2.2 purger')
-hdlr = logging.FileHandler( PURGE_LOG )
+hdlr = logging.FileHandler(PURGE_LOG)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
+logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 # Logging to stdout
@@ -72,9 +71,9 @@ def jobs_to_delete(days):
     command = [TQ, 'jobs',
                'not active and not ready and spooltime  < -' + days + 'd',
                '--noheader', '--archives', '-c', 'jid']
-    p = subprocess.Popen( command, stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT )
-    
+    p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+
     try:
         for line in iter(p.stdout.readline, b''):
             sys.stdout.flush()
@@ -91,13 +90,13 @@ def get_all_job_folders(cmd_logs_dir):
     """Create list of all job folders
     """
     job_folders = []
-    for root, directories, files in os.walk( cmd_logs_dir ):
+    for root, directories, files in os.walk(cmd_logs_dir):
         if len(directories) > 0:
             for directory in directories:
                 match = re.search('J\d*', directory)
                 if match:
                     job_folder = root + '/' + directory
-                    job_folders.append( job_folder )
+                    job_folders.append(job_folder)
     return job_folders
 
 
@@ -108,13 +107,12 @@ def get_job_deletion_list(job_folders, jids):
     for job_folder in job_folders:
         jid_match = False
         for jid in jids:
-            if job_folder.endswith('J'+str(jid)):
+            if job_folder.endswith('J' + str(jid)):
                 jid_match = True
-            
+
         if jid_match:
-            delete_list.append( job_folder )
+            delete_list.append(job_folder)
             logger.info('Added log folder for deletion: ' + job_folder)
-            
 
     return delete_list
 
@@ -124,10 +122,10 @@ def delete_logs(delete_list):
     """
     for job_folder in delete_list:
         if not DRY_RUN:
-            logger.info( 'Deleting ' + job_folder )
-            shutil.rmtree( job_folder )
+            logger.info('Deleting ' + job_folder)
+            shutil.rmtree(job_folder)
         else:
-            logger.info( 'Dry run: (not) deleting ' + job_folder )
+            logger.info('Dry run: (not) deleting ' + job_folder)
 
 
 def delete_tractor_jobs(days):
@@ -135,24 +133,22 @@ def delete_tractor_jobs(days):
     tractor-dbctl --purge-archive-to-year-month YY-MM
     """
     if not DRY_RUN:
-        logger.info( 'Executing tq command to delete jobs...' )
+        logger.info('Executing tq command to delete jobs...')
         command = [TQ, '--force', '--yes', 'delete',
                    'not active and not ready and spooltime  < -' + days + 'd']
     else:
-        logger.info( 'Executing tq command to (not) delete jobs...' )
+        logger.info('Executing tq command to (not) delete jobs...')
         command = [TQ, 'jobs', '--archives',
                    'not active and not ready and spooltime  < -' + days + 'd']
 
-    p = subprocess.Popen( command, stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT )
+    p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
     try:
         for line in iter(p.stdout.readline, b''):
             sys.stdout.flush()
-            logger.info( line.rstrip() )
+            logger.info(line.rstrip())
     except:
         logger.warning('Failed reading stdout.')
-
-
 
 
 ####################################
@@ -161,9 +157,9 @@ def delete_tractor_jobs(days):
 if __name__ == '__main__':
 
     if not DRY_RUN:
-        logger.info( 'Tractor purge initiated.' )
+        logger.info('Tractor purge initiated.')
     else:
-        logger.info( 'Tractor purge initiated in "dry run" mode.' )
+        logger.info('Tractor purge initiated in "dry run" mode.')
 
     # Queries
     jids = jobs_to_delete(days=DAYS)
@@ -175,18 +171,17 @@ if __name__ == '__main__':
     logger.info('Job log folders found: ' + str(len(job_folders)))
     logger.info('Job log folders to be deleted: ' + str(len(delete_list)))
 
-
     # Delete logs
     if len(jids) > 0:
         delete_logs(delete_list=delete_list)
     else:
-        logger.info( 'No logs to delete.' )
+        logger.info('No logs to delete.')
 
     # Delete jobs
     if DELETE_JOBS:
         if len(delete_list) > 0:
             delete_tractor_jobs(days=DAYS)
         else:
-            logger.info( 'No jobs to delete.' )
+            logger.info('No jobs to delete.')
 
-    logger.info( 'Tractor purge done.\n' )
+    logger.info('Tractor purge done.\n')
